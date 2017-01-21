f0864cc8693f3873c803ba5f18fbfb1ee18a8395
sndnvaps <sndnvaps@gmail.com>
2017-01-21 11:02:34 +0800

    use config.yaml to deal with botfather   use @botname whoami to get the userid of you
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- slackbot.go
+++ slackbot.go
@@ -11,6 +11,21 @@ import (
 
 var botID = "N/A"     // U2NQSPHHD bender bot userID
 var channelID = "N/A" // C3K9VAK3N
+//var botFather = "" // use @botname whoami, to get the user_id of bot_father
+
+func GetBotFatherId(filename string) string {
+	cfg, err := config.ParseYamlFile(filename)
+	if err != nil {
+		fmt.Printf("Cannot parse the config file: %s\n", filename)
+	}
+	botfather := cfg.UString("botfather", "")
+	if botfather == "" {
+		fmt.Printf("Get botfather from env\n")
+		botfather = os.Getenv("botfather")
+	}
+
+	return botfather
+}
 
 func GetToken(filename string) string {
 	cfg, err := config.ParseYamlFile(filename)
@@ -57,15 +72,6 @@ func ParseCommand(api *slack.Client, rtm *slack.RTM, cmd []string, userid string
 		attachment := slack.Attachment{
 			Pretext: stock_info,
 			Text:    stock_mes,
-			// Uncomment the following part to send a field too
-			/*
-				Fields: []slack.AttachmentField{
-					slack.AttachmentField{
-							Title: "a",
-							Value: "no",
-							},
-						},
-			*/
 		}
 		params.Attachments = []slack.Attachment{attachment}
 		channelID, timestamp, err := api.PostMessage(channelid, "stock ", params)
@@ -145,8 +151,8 @@ func ParseCommand(api *slack.Client, rtm *slack.RTM, cmd []string, userid string
 		fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 
 	} else if len(cmd) == 2 && (cmd[1] == "botfather" || cmd[1] == "father" || cmd[1] == "bot_father") {
-
-		user, err := api.GetUserInfo("U3UP1QT8E")
+		botfather := GetBotFatherId("config.yaml") // get botfathe userid from config.yaml
+		user, err := api.GetUserInfo(botfather)
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			return err
