21b5aa9b7cf58d397b28f9c767eaa6f8b3bde097
sndnvaps <sndnvaps@gmail.com>
2017-01-26 18:04:26 +0800

    return random pic from the local path
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- upload/main.go
+++ upload/main.go
@@ -4,17 +4,45 @@ import (
 	"fmt"
 
 	"github.com/nlopes/slack"
+	"io/ioutil"
 	"os"
+	"strings"
 )
+
+//add *.jpg| *.jpeg|*.png|*.gif to -> []string, as return
+func ListPath(path string) []string {
+	var dl_out []string
+	basePath, _ := os.Getwd() //+ "/" + path
+	dir_list, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println("read dir error ", err.Error())
+	}
+	for _, key := range dir_list {
+		//fmt.Println(index, "=", key.Name())
+		if strings.HasSuffix(key.Name(), ".jpg") ||
+			strings.HasSuffix(key.Name(), ".jpeg") ||
+			strings.HasSuffix(key.Name(), ".png") ||
+			strings.HasSuffix(key.Name(), ".gif") {
+			dl_out = append(dl_out, basePath+"/"+path+"/"+key.Name())
+		}
+	}
+	return dl_out
+}
+
+func RandomPicName(files []string) string {
+	index := RandInt(0, len(files))
+	return files[index]
+}
 
 func main() {
 	var token string = os.Getenv("slacktoken")
+	dir_list := ListPath("images")
 	channel := []string{"#general"}
 	api := slack.New(token)
 	params := slack.FileUploadParameters{
 		Title:    "Batman Example",
 		Filetype: "auto",
-		File:     "example.txt", // *.jpg| *.jpeg|*.png
+		File:     RandomPicName(dir_list), // *.jpg| *.jpeg|*.png|*.gif
 		Channels: channel,
 
 		//Content:  "Nan Nan Nan Nan Nan Nan Nan Nan Batman",
